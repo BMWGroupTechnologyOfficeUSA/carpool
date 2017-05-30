7a88b3076dd95ea26c919de0ed280cc5dfe77803
Douglas Hewitt <doughewitt4@gmail.com>
2017-05-30 16:37:13 -0700

    setup carthage and realm

---
+++ carpool-iOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  carpool-iOS
+//
+//  Created by Douglas Hewitt on 5/30/17.
+//  Copyright © 2017 BMW. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
